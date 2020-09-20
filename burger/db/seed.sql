d2ab9fb8dc34360dd639bd54e609335b0769bfc8
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-20 12:30:32 -0400

    added seed

---
+++ burger/db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+('Black Bean Burger', false),
+('Carolina Burger', true),
+('Sweet Potato and Quinoa Burger', false),
+('Thai Veggie Burger', true);
