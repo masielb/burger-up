cff6292663b62606e0a907eea15d00faf18c6a7e
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-21 18:27:00 -0400

    updated routes

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -4,5 +4,35 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", (req, res) => {
+	burger.all((data) => {
+		var hbsObj = { burger: data };
+		res.render("index", hbsObj);
+	});
+});
+
+// Post Route: Add Burger
+router.post("/api/burger", (req, res) => {
+	burger.insertOne(
+		// → orm.js insertOne
+		["burger_name", "devoured"],
+		// add burger_name and devoured=false
+		[req.body.burger_name, req.body.devoured],
+		(result) => {
+
+            res.redirect("/");
+		},
+	);
+});
+
+// Get burger to update
+router.get("/api/burger/update", (req, res) => {
+	burger.updateOne(req.query.id, { devoured: req.query.devoured }, (result) => {
+		res.redirect("/");
+	});
+
+});
+
 // Export routes for server.js to use
 module.exports = router;
