e151abe2aa1376a45faac7bc099268ed449767f5
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-21 20:50:27 -0400

    updated burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,22 @@
 //Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+const orm = require("../config/orm.js");
 
-// module.export = burger.js idk
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res);
+        });
+    },
+    updateOne: function(cols, condition, cb) {
+        orm.updateOne("burgers", cols, condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.export = burger
