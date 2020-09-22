f11ad7df9917c83ca2161bfea8be1c1f19038c2c
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-21 20:23:57 -0400

    reorganized files

--- burger/db/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO burgers (burger_name, devoured)
-VALUES 
-('Black Bean Burger', false),
-('Carolina Burger', false),
-('Sweet Potato and Quinoa Burger', false),
-('Thai Veggie Burger', false);
-
-SELECT * FROM burgers;
