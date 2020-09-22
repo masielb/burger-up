d8ce0a7385cd0e81f4bdea150d222b488d1cfef5
Masiel Bautista <masiel5@users.noreply.github.com>
2020-09-21 20:21:11 -0400

    removed burger folder

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+('Black Bean Burger', false),
+('Carolina Burger', false),
+('Sweet Potato and Quinoa Burger', false),
+('Thai Veggie Burger', false);
+
+SELECT * FROM burgers;
